5bb53aaebc5942455d5dc652ed948b37b9ac2131
WrinePhantom <74916765+WrinePhantom@users.noreply.github.com>
2023-05-23 22:28:10 +0300

    FundementalOfGamePart2
    cubes do not allowed to put on itself
    There is Trash box when you drag on it

--- Assets/MKaan/Scripts/Draggable.cs
+++ Assets/MKaan/Scripts/Draggable.cs
@@ -5,8 +5,9 @@ using UnityEngine.EventSystems;
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [HideInInspector] public static bool releasedTetromino = false;
+    bool releasedtetromino2 = false;
 
-    Collider2D ItemSlotsCollider;
+    public GameObject ItemSlotsCollider;
 
     Draggable draggable;
 
@@ -14,13 +15,14 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
     bool firstPick;
     public static bool InArea;
+    bool InArea2;
 
     public GameObject LeftBottomCornerBound;
     public GameObject RightTopCornerBound;
 
     void Start()
     {
-        ItemSlotsCollider = GetComponentInChildren<Collider2D>();
+        ItemSlotsCollider = GameObject.FindWithTag("Canvas");
         draggable = GetComponent<Draggable>();
         currentPosition = transform.position;
     }
@@ -40,23 +42,38 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
             && RightTopCornerBound.transform.position.y < 970)
         {
             InArea = true;
+            InArea2 = true;
         }
         else
+        {
             InArea = false;
+            InArea2 = false;
+        }
+            
     }
-
     void Update()
     {
         if(firstPick == true)
             ManuelDetector();
-        if (ItemSlotsCollider.enabled == false)
+
+        if(releasedtetromino2 == true && InArea2 == true && FilledSlot == false)
+        {
             draggable.enabled = false;
+        }
+        //print(FilledSlot);
+
+        if (InArea2 == false && FilledSlot2 == true)
+        {
+            FilledSlot = false;
+            FilledSlot2 = false;
+        }
     }
   
     public void OnBeginDrag(PointerEventData eventData)
     {
         firstPick = true;
         releasedTetromino = false;
+        releasedtetromino2 = false;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -64,13 +81,51 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
         transform.position = Input.mousePosition;
     }
 
+    public static bool MakeItDisactive;
     public void OnEndDrag(PointerEventData eventData)
     {
         releasedTetromino = true;
+        releasedtetromino2 = true;
         firstPick = false;
 
-        if(InArea == false)
-            transform.position = currentPosition;
+        if(InArea == false || FilledSlot == true)
+        {
+            //MakeItDisactive = true;
+            //transform.position = currentPosition;
+            Destroy(gameObject,0.1f);
+            Instantiate(gameObject, currentPosition, Quaternion.identity,ItemSlotsCollider.transform);
+        }//
+        
+            
+        //
+        if(InArea == true)
+        {
+            gameObject.tag = "Filled";
+        }
+        //
     }
-    
+    public static bool FilledSlot;//
+    bool FilledSlot2;
+
+    private void OnTriggerStay2D(Collider2D collision)//
+    {
+        if (collision.gameObject.tag == "Filled")
+        {
+            FilledSlot = true;
+            FilledSlot2 = true;
+        }
+        if (collision.gameObject.tag == "Destroy")
+        {
+            Destroy(gameObject,0.25f);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)//
+    {
+        if (collision.gameObject.tag == "Filled")
+        {
+            FilledSlot = false;
+            FilledSlot2 = false;
+        }
+    }//
+
 }
